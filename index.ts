20f3df7b83304f26523145e640afdfa22fdd3941
unknown <ankit1738@gmail.com>
2025-02-10 23:43:16 +0530

    Added a sample get route for testing

--- index.ts
+++ index.ts
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+})
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
